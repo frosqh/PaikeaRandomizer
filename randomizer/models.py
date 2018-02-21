03384c657a3ab089e6b10a75efade4436d235fa3
Romain Brandy <romain.brandy@orange.fr>
2018-02-21 17:00:23 +0100

    Ajout du modèle de la base de données.
    Signed-off-by: Romain Brandy <romain.brandy@orange.fr>

--- randomizer/models.py
+++ randomizer/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+def user_directory_path(instance, filename):
+    return settings.MEDIA_ROOT+'/'+instance.author.authorName+'/'+filename
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
-    def __str__(self):
-        return self.title
+class Game(models.Model):
+    name = models.CharField(max_length=50, default='Game')
+    speed = models.ForeignKey(Speed, on_delete=models.CASCADE)
+    length = models.ForeignKey(Length, on_delete=models.CASCADE)
+    logo = models.ImageField(upload_to=user_directory_path)
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=20, default='Roger')
+
+
+class Speed(models.Model):
+    name = models.CharField(max_length=20, default='Normal')
+
+
+class Length(models.Model):
+    name = models.IntegerField(default=0)
+
+
+class GameByPlayer(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
